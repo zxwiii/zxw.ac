d6c2e6672d14edc983a1825cbcae6b9a691e879e
zxwiii <61055170+zxwiii@users.noreply.github.com>
2022-11-04 00:06:39 +0800

    Update oauth_environment_variables.rb

--- graph-sample/config/oauth_environment_variables.rb
+++ graph-sample/config/oauth_environment_variables.rb
@@ -1,3 +1,3 @@
 ENV['AZURE_APP_ID'] = 'a1094e79-8cfd-4f96-9588-14daeabfe723'
-ENV['AZURE_APP_SECRET'] = 'YOUR_APP_SECRET_HERE'
+ENV['AZURE_APP_SECRET'] = 'UIX8Q~Nla4wcX7g0uEgdblb8vuxxZck9oVauNaAY'
 ENV['AZURE_SCOPES'] = 'openid profile email offline_access user.read mailboxsettings.read calendars.readwrite'
